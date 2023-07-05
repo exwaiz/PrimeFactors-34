41181d4bb8d4c318c73c2a33968182238bae8021
dongjune.youn <exwaiz@live.com>
2023-07-05 15:17:13 +0900

    [feature] 37p

--- googleTest/test.cpp
+++ googleTest/test.cpp
@@ -3,8 +3,14 @@
 
 using namespace std;
 
-TEST(TestCaseName, Of1) {
+TEST(PrimeFactors, Of1) {
 	PrimeFactor prime_factor;
 	vector<int> expected = { };
 	EXPECT_EQ(expected, prime_factor.of(1));
+}
+
+TEST(PrimeFactors, Of2) {
+	PrimeFactor prime_factor;
+	vector<int> expected = {2};
+	EXPECT_EQ(expected, prime_factor.of(2));
 }
--- tdd/prime_factors.cpp
+++ tdd/prime_factors.cpp
@@ -5,9 +5,12 @@ using namespace std;
 class PrimeFactor
 {
 public:
-	vector<int>of(int i)
+	vector<int>of(int number)
 	{
 		vector<int> result = {  };
-		return result;
+		if (number == 2){
+			result.push_back((2));
+		}
+;		return result;
 	}
 };
